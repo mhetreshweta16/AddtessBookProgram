4544587f95193501f6ee90cc89dd7c09596b0211
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-15 15:18:40 +0530

    [shweta] added the code of adding new contact

--- AddressBook/AddressBookClass.cs
+++ AddressBook/AddressBookClass.cs
@@ -16,58 +16,55 @@ namespace AddressBook
         int zipCode;
         long phoneNumber;
        string email;
-        //ArrayList arlist = new ArrayList();
+        ArrayList arlist = new ArrayList();
         public void GetContactDetails()   // creating contact details of person
         {
            
             Console.WriteLine("Enter the First Name");
             firstName = Console.ReadLine();
-           // arlist.Add(firstName);
+          arlist.Add(firstName);
 
             Console.WriteLine("Enter the Last Name");
            lastName = Console.ReadLine();
-           // arlist.Add(lastName);
+           arlist.Add(lastName);
 
             Console.WriteLine("Enter the Adresss");
             address[0] = Console.ReadLine();
-           // arlist.Add(address[0]);
+           arlist.Add(address[0]);
 
 
             Console.WriteLine("Enter the State");
              state = Console.ReadLine();
-            //   arlist.Add(state);
+               arlist.Add(state);
 
             Console.WriteLine("Enter the Zip Code");
              zipCode = Convert.ToInt32(Console.ReadLine());
-            //arlist.Add(zipCode);
+            arlist.Add(zipCode);
 
             Console.WriteLine("Enter the Phone Number");
-            // int[] phoneNumber = new int[10];
+            
              phoneNumber = Convert.ToInt64(Console.ReadLine());
-   //         arlist.Add(phoneNumber);
+             arlist.Add(phoneNumber);
 
             Console.WriteLine("Enter the Email");
              email = Console.ReadLine();
-     //       arlist.Add(email);
+               arlist.Add(email);
 
-
+           // arlist.AddRange(firstName, lastName, address, state, zipCode, phoneNumber, email);
         }
 
         public void  putDetails() // displaying contact details
         {
-            Console.WriteLine("First Name:" + firstName);
-            Console.WriteLine("Last Name:" + lastName);
-            Console.WriteLine("Address:" + address[0]);
-            Console.WriteLine("state:" + state);
-            Console.WriteLine("Zip Code:" + zipCode);
-            Console.WriteLine("Phone NUmber:" + phoneNumber);
-            Console.WriteLine("Email" + email);
+           
+            Console.WriteLine("\n====The Contact Details===\n");
 
-           /* for (int i = 0; i < arlist.Count; i++)
+            for (int i = 0; i < arlist.Count; i++)
             {
-                Console.WriteLine("\n====The Contact Details===\n");
+                
                 Console.Write(arlist[i] + "  ");
-            }  */
+                Console.WriteLine("\n");
+                
+            }  
 
         }
 
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,15 +6,52 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
-           
-
             AddressBookClass bookClass = new AddressBookClass(); // creating object of class
+            string yes = "y";
+            string y;
 
-            bookClass.GetContactDetails();   //through object accessing method 
-            bookClass.putDetails();
+            do
+            {
+                Console.WriteLine("Welcome to Address Book");
+                Console.WriteLine("1.AddNewContact\n2.ShowContact\n3.EditContact\n4.RmoveContact");
+                Console.WriteLine("\nEnter your choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
 
 
+                switch (ch)
+                {
+
+                    case 1:
+                        Console.WriteLine("\nhow many contact you want to add");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                        {
+                            bookClass.GetContactDetails();
+                            Console.WriteLine("\n");    //through object accessing method 
+
+                        }
+                        break;
+                    case 2:
+                        bookClass.putDetails();
+                        break;
+
+                    case 3:
+                        /// Edit Contact
+                        break;
+
+                    case 4:
+                        /// Remove Contact
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("do you want to continue? press...y/n");
+                y = Console.ReadLine();
+
+            } while (yes == y);
+
+           Console.ReadLine();
 
 
         }
